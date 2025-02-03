63641636b0db63d28ec30b162e5c320a5fb67316
Isuru-Pramod <mdipromod@gmail.com>
2025-02-03 22:42:37 +0530

    home page adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import AdminPage from './pages/admin/adminpage.jsx'
+import HomePage from './pages/home/homePage.jsx'
+
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes path="/*">
+        <Route path="/admin/*" element={<AdminPage/>}/>
+        <Route path="/*" element={<HomePage/>}/>
+
+      </Routes>
+    </BrowserRouter>
+
+  ) 
 }
 
 export default App
